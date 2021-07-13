d68198a7e4cf4b006f3ddb293c9e6dabbf9f9238
LabyrinthLion <66320873+LabyrinthLion@users.noreply.github.com>
2021-07-13 17:10:06 +0100

    Update serverless func

--- src/pages/api/updateVideoDb.js
+++ src/pages/api/updateVideoDb.js
@@ -1,36 +1,38 @@
-const fetch = require('node-fetch');
-const fs = require('fs');
-const env = require('../../scripts/env');
-fetch(
-    `https://www.googleapis.com/youtube/v3/channels?key=${env.API_KEY}&id=${env.CHANNEL_ID}&part=contentDetails`
-)
-    .then(async (results) => {
-        const channelObj = await results.json();
-        fetch(
-            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId=${channelObj.items[0].contentDetails.relatedPlaylists.uploads}&key=${env.API_KEY}`
-        )
-            .then(async (results) => {
-                const videosObj = await results.json();
-                fs.writeFile(
-                    './public/data/videos.json',
-                    JSON.stringify(videosObj),
-                    'utf8',
-                    function (err) {
-                        if (err) {
-                            console.log(
-                                'Failed to update video database.',
-                                err
-                            );
-                        } else {
-                            console.log('Updated video database.');
+module.exports = (req, res) => {
+    const fetch = require('node-fetch');
+    const fs = require('fs');
+    const env = require('../../scripts/env');
+    fetch(
+        `https://www.googleapis.com/youtube/v3/channels?key=${env.API_KEY}&id=${env.CHANNEL_ID}&part=contentDetails`
+    )
+        .then(async (results) => {
+            const channelObj = await results.json();
+            fetch(
+                `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId=${channelObj.items[0].contentDetails.relatedPlaylists.uploads}&key=${env.API_KEY}`
+            )
+                .then(async (results) => {
+                    const videosObj = await results.json();
+                    fs.writeFile(
+                        './public/data/videos.json',
+                        JSON.stringify(videosObj),
+                        'utf8',
+                        function (err) {
+                            if (err) {
+                                console.log(
+                                    'Failed to update video database.',
+                                    err
+                                );
+                            } else {
+                                console.log('Updated video database.');
+                            }
                         }
-                    }
-                );
-            })
-            .catch((err) => {
-                console.log('Failed to update video database.', err);
-            });
-    })
-    .catch((err) => {
-        console.log('Failed to update video database.', err);
-    });
+                    );
+                })
+                .catch((err) => {
+                    console.log('Failed to update video database.', err);
+                });
+        })
+        .catch((err) => {
+            console.log('Failed to update video database.', err);
+        });
+};
